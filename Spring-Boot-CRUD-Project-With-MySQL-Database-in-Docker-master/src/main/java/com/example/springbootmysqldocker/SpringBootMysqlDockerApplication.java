318a8c3a63db18e3bf08b91088ba2b96562b79df
monika5417 <159286874+monika5417@users.noreply.github.com>
2025-09-15 17:22:20 +0530

    added extends SpringBootServletInitializer in main file and change application.properties file

--- Spring-Boot-CRUD-Project-With-MySQL-Database-in-Docker-master/src/main/java/com/example/springbootmysqldocker/SpringBootMysqlDockerApplication.java
+++ Spring-Boot-CRUD-Project-With-MySQL-Database-in-Docker-master/src/main/java/com/example/springbootmysqldocker/SpringBootMysqlDockerApplication.java
@@ -3,9 +3,10 @@ package com.example.springbootmysqldocker;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
 @SpringBootApplication
-public class SpringBootMysqlDockerApplication {
+public class SpringBootMysqlDockerApplication extends SpringBootServletInitializer {
 
 
 	public static void main(String[] args) {
